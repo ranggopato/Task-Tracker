bb4c95bed85856bbcbb23e3140c74dfb19b6ea32
ranggo pato <patoranggo@gmail.com>
2022-11-26 16:30:12 +0700

    task tracker first commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,39 @@
+import { Task } from './Task';
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Meeting with client',
+    day: 'November 29th at 2.30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Belanja bulanan',
+    day: 'November 29th at 2.30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Konsultasi ke dokter',
+    day: 'November 30th at 2.30pm',
+    reminder: true,
+  },
+  {
+    id: 4,
+    text: 'Meeting di kantor',
+    day: 'November 31th at 2.30pm',
+    reminder: true,
+  },
+  {
+    id: 5,
+    text: 'Pertemuan kerja',
+    day: 'December 1th at 2.30pm',
+    reminder: true,
+  },
+  {
+    id: 6,
+    text: 'Acara sekolah diluar',
+    day: 'December 2th at 2.30pm',
+    reminder: true,
+  },
+];
